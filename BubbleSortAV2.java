949eedd29104018637061b45740d43054144807f
Felipe Peixoto <felipepeixoto33@hotmail.com>
2023-10-30 08:12:24 -0300

    Update BubbleSortAV2.java

--- BubbleSortAV2.java
+++ BubbleSortAV2.java
@@ -61,22 +61,6 @@ public class BubbleSortAV2 {
             }
         }
     }
-
-    public static void bubbleSort(int[] arr) {
-        double initTime = System.currentTimeMillis();
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        double endTime = System.currentTimeMillis();
-        System.out.println("Serial Time: " + (endTime-initTime)/1000);
-    }
 }
 
 class Essentials {
